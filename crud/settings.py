47d18a0e4f0b580c2681c251623c809ccf5028a5
jeonseongjae98 <jeonseongjae@naver.com>
2023-04-19 17:13:50 +0900

    commit

--- crud/settings.py
+++ crud/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-cloudtest-e9btb72blgndxnug.sel3.cloudtype.app']
 
 
 # Application definition
